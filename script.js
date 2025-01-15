5ee297aac2862c9653da04118a2c5d1e59caf3d4
Wdson rafael <wdson3souza@gmail.com>
2025-01-14 21:01:16 -0400

    estilização do portifolio

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ function toggleMode() {
   }*/
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/wdson2-light.jpg")
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/wdson.jpg")
   }
 }
